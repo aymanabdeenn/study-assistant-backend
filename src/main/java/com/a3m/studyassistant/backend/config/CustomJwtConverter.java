65ac2d81ea6b8173268931d673e53412eac6fe11
aymanabdeenn <151556377+aymanabdeenn@users.noreply.github.com>
2026-03-16 14:22:29 +0300

    feat: Got the connection with supabase working along with the authentication

--- src/main/java/com/a3m/studyassistant/backend/config/CustomJwtConverter.java
+++ src/main/java/com/a3m/studyassistant/backend/config/CustomJwtConverter.java
@@ -30,7 +30,7 @@ public class CustomJwtConverter implements Converter<Jwt, AbstractAuthentication
             String role = (String) appMetadata.get("role"); // e.g., "ADMIN"
 
             // 3. Add the ROLE_ prefix so @PreAuthorize("hasRole('ADMIN')") works
-            GrantedAuthority customRole = new SimpleGrantedAuthority("ROLE_" + role);
+            GrantedAuthority customRole = new SimpleGrantedAuthority("ROLE_" + role.toUpperCase());
 
             // Combine standard scopes with your custom role
             authorities = Stream.concat(authorities.stream(), Stream.of(customRole))
@@ -41,4 +41,26 @@ public class CustomJwtConverter implements Converter<Jwt, AbstractAuthentication
         // We use jwt.getSubject() (the UUID) as the "name" of the user
         return new JwtAuthenticationToken(jwt, authorities, jwt.getSubject());
     }
+
+//    @Override
+//    public AbstractAuthenticationToken convert(Jwt jwt) {
+//        // 1. Get standard authorities (like SCOPE_read)
+//        Collection<GrantedAuthority> authorities = defaultAuthoritiesConverter.convert(jwt);
+//
+//        // 2. Extract custom role from Supabase 'app_metadata'
+//        Map<String, Object> appMetadata = jwt.getClaim("app_metadata");
+//
+//        String role = (appMetadata != null && appMetadata.containsKey("role")) ? (String) appMetadata.get("role") : "SERVICE";
+//
+//        // 3. Add the ROLE_ prefix so @PreAuthorize("hasRole('ADMIN')") works
+//        GrantedAuthority customRole = new SimpleGrantedAuthority("ROLE_" + role.toUpperCase());
+//
+//        // Combine standard scopes with your custom role
+//        authorities = Stream.concat(authorities.stream(), Stream.of(customRole))
+//                    .collect(Collectors.toSet());
+//
+//        // 4. Return the Token object that Spring puts in the SecurityContext
+//        // We use jwt.getSubject() (the UUID) as the "name" of the user
+//        return new JwtAuthenticationToken(jwt, authorities, jwt.getSubject());
+//    }
 }
--- src/main/java/com/a3m/studyassistant/backend/config/SecurityConfig.java
+++ src/main/java/com/a3m/studyassistant/backend/config/SecurityConfig.java
@@ -1,14 +1,35 @@
 package com.a3m.studyassistant.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public JwtDecoder jwtDecoder(
+            @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}") String secret
+            , @Value("${jwt.algorithm}") String algorithm
+    ) {
+        String javaAlg = switch (algorithm.toUpperCase()) {
+            case "HS256" -> "HmacSHA256";
+            case "HS384" -> "HmacSHA384";
+            case "HS512" -> "HmacSHA512";
+            default -> throw new IllegalArgumentException("Unsupported algorithm: " + algorithm);
+        };
+
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(), javaAlg);
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
