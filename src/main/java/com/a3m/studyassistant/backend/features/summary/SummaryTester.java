3343e67bcf6f5685e357a5d591295b23e4804e6a
aymanabdeenn <151556377+aymanabdeenn@users.noreply.github.com>
2026-05-03 14:04:24 +0300

    fix: Commented out \@Component in SummaryTester.

--- src/main/java/com/a3m/studyassistant/backend/features/summary/SummaryTester.java
+++ src/main/java/com/a3m/studyassistant/backend/features/summary/SummaryTester.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
+//@Component
 public class SummaryTester implements CommandLineRunner {
 
     private final SummaryService summaryService;
