bd08f1330d34a44d5556ec98529ce34491b7d759
aymanabdeenn <151556377+aymanabdeenn@users.noreply.github.com>
2026-05-08 11:43:47 +0300

    fix: Commented out the component annotation in QuizTester.

--- src/main/java/com/a3m/studyassistant/backend/features/quiz/QuizTester.java
+++ src/main/java/com/a3m/studyassistant/backend/features/quiz/QuizTester.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.UUID;
 
-@Component
+//@Component
 public class QuizTester implements CommandLineRunner {
 
     private final QuizService quizService;
